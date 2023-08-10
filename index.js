623b2222aa8415386a269aa93e6ab3172ebf7893
harshshukla2002 <harshshukla2312@gmail.com>
2023-08-10 20:36:39 +0530

    gleams-backend

---
+++ index.js
@@ -0,0 +1,22 @@
+const jsonServer = require("json-server");
+const cors = require("cors");
+const path = require("path");
+const fs = require("fs");
+
+const server = jsonServer.create();
+const db = JSON.parse(fs.readFileSync(path.join(__dirname, "db.json")));
+const router = jsonServer.router(db);
+const middleware = jsonServer.defaults();
+
+server.use(cors());
+server.use(jsonServer.bodyParser);
+server.use(middleware);
+server.use(router);
+
+server.listen(8000, () => {
+  try {
+    console.log("server is running");
+  } catch (error) {
+    console.log(error);
+  }
+});
